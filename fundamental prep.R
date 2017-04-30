98c7fdc4f7f77c8b1dff01b5a5c12b53cbdba02e
McFortran <samuelmgifford@gmail.com>
2017-04-30 16:13:54 -0500

    Added historical summary columns

--- fundamental prep.R
+++ fundamental prep.R
@@ -1,6 +1,6 @@
 #Read Fundamental Data - core US premium
 #' @note appends delisted companies with a numeric index if the ticker is recycled
-fun <- fread("~/Core_US_Fundamentals/fun_4_23_2017.csv")
+fun <- fread("~/Core_US_Fundamentals/fun_4_23_2017.csv",integer64="numeric")
 fun[,Date:=as.Date(datekey,"%Y-%m-%d")]
 fun[,calendardate:=as.Date(calendardate,"%Y-%m-%d")]
 funtick <- fread("~/Core_US_Fundamentals/tickers.txt",select=c("Ticker","Name","SIC"))
@@ -8,27 +8,31 @@ fun <- merge(fun,funtick,by.x="ticker",by.y="Ticker")
 fun[,ticker:=gsub("[.-]","",ticker)] #Core US removes . and - from their ticker
 fun[,tick:=paste0(ticker,SIC)]
 setkeyv(fun,c("ticker","calendardate","datekey"))
-restatement_fields <- c(assets="assets",balance_sheet="bvps",assets_short="currentratio",leverage="de",debt="debt",operating_earnings="ebit",equity="equity",cash_flow="fcf",profit="gp",
-                        investments="investments",liabilities="liabilities",operating_cash_flow="ncfo",income="netinc",payables="payables",receivables="receivables",revenue="revenue",
-                        tangibles="tangibles",intangibles="intangibles",inventory="inventory",working_capital="workingcapital")
+
+#Add ratios
+
+
+#Prep financial restatements
+restatement_fields <- c("assets","assetsc","assetsnc","bvps","cashneq","cor","currentratio","de","debt","depamor","ebit","equity","fcf","gp",
+                        "investments","liabilities","ncfo","netinc","payables","receivables","revenue",
+                        "tangibles","intangibles","inventory","workingcapital")
 rsdc <- c("dimension",restatement_fields)
-begin <- proc.time()
 q_restatement <- fun[dimension%in%c("ARQ","MRQ"), lapply(.SD,calc_restatement_generator_quarterly(dimension)) ,keyby=.(ticker,calendardate),.SDcols=rsdc]
 y_restatement <- fun[dimension%in%c("ARY","MRY"), lapply(.SD,calc_restatement_generator_yearly(dimension)) ,keyby=.(ticker,calendardate),.SDcols=rsdc]
-end <- proc.time()
-print(end-begin)
 q_restatement[,dimension:=NULL]
 names(q_restatement) <- c("ticker","calendardate",paste0("re_diff_",restatement_fields))
 y_restatement[,dimension:=NULL]
 names(y_restatement) <- c("ticker","calendardate",paste0("re_diff_",restatement_fields))
+
+#restatement - quarterly
 funq <- fun[dimension=="ARQ"]
 funq <- merge(funq,q_restatement,by=c("ticker","calendardate"))
-rasset <- normalize_fields(paste0("re_diff_",c("assets","investments","tangibles","intangibles")),"assets",funq)
-rquick <- normalize_fields(paste0("re_diff_",c("receivables","payables","inventory","workingcapital")),"workingcapital",funq)
+rasset <- normalize_fields(paste0("re_diff_",c("assets","assetsnc","cashneq","investments","tangibles","intangibles")),"assets",funq)
+rquick <- normalize_fields(paste0("re_diff_",c("assetsc","receivables","payables","inventory","workingcapital")),"workingcapital",funq)
 rliab <- normalize_fields(paste0("re_diff_",c("debt","liabilities")),"liabilities",funq)
 requity <- normalize_fields(paste0("re_diff_",c("equity")),"equity",funq)
 rcash <- normalize_fields(paste0("re_diff_",c("fcf","ncfo")),"ncfo",funq)
-rincome <- normalize_fields(paste0("re_diff_",c("ebit","gp","netinc","revenue")),"revenue",funq)
+rincome <- normalize_fields(paste0("re_diff_",c("cor","depamor","ebit","gp","netinc","revenue")),"revenue",funq)
 rfields <- c(rasset,rquick,rliab,requity,rcash,rincome)
 nqtrs <- 12
 qdiff_fields <- c()
@@ -39,8 +43,33 @@ for(i in 1:length(rfields)) {
     qdiff_fields <- c(qdiff_fields,nm)
   }
 }
-chg_fields <- c("assets","bvps","capex","currentratio","de","dps","ebit","ebitda","ebitdamargin","eps","equity","fcf","gp","grossmargin","inventory","investments","liabilities","ncf","ncfo","netinc","opex","opinc","payables",
-                "receivables","revenue","roa","roe","roic","ros","tbvps","workingcapital")
+
+# #restatement - yearly
+# funy <- fun[dimension=="ARY"]
+# funy <- merge(funy,y_restatement,by=c("ticker","calendardate"))
+# rasset <- normalize_fields(paste0("re_diff_",c("assets","assetsnc","cashneq","investments","tangibles","intangibles")),"assets",funq)
+# rquick <- normalize_fields(paste0("re_diff_",c("assetsc","receivables","payables","inventory","workingcapital")),"workingcapital",funq)
+# rliab <- normalize_fields(paste0("re_diff_",c("debt","liabilities")),"liabilities",funq)
+# requity <- normalize_fields(paste0("re_diff_",c("equity")),"equity",funq)
+# rcash <- normalize_fields(paste0("re_diff_",c("fcf","ncfo")),"ncfo",funq)
+# rincome <- normalize_fields(paste0("re_diff_",c("cor","depamor","ebit","gp","netinc","revenue")),"revenue",funq)
+# rfields <- c(rasset,rquick,rliab,requity,rcash,rincome)
+# nyrs <- 3
+# ydiff_fields <- c()
+# for(i in 1:length(rfields)) {
+#   for(j in 1:nyrs) {
+#     nm <- paste0(rfields[i],"_",j,"_yr")
+#     funy <- lookbehindqtr(nm,rfields[i],j*4,funy)
+#     ydiff_fields <- c(ydiff_fields,nm)
+#   }
+# }
+
+
+#Add prior fields
+chg_fields <- c("assets","assetsc","assetsnc","assetturnover","bvps","capex","cashneq","cor","currentratio","de","debt","deferredrev","depamor",
+                "dps","ebit","ebitda","ebitdamargin","ebt","eps","equity","fcf","gp","grossmargin","intangibles","intexp","invcap","inventory",
+                "investments","liabilities","liabilitiesc","liabilitiesnc","ncf","ncfdebt","ncff","ncfi","ncfo","netinc","opex","opinc","payables",
+                "receivables","retearn","revenue","rnd","roa","roe","roic","ros","sgna","tangibles","tbvps","workingcapital")
 qchg_fields <- c()
 for(i in 1:length(chg_fields)) {
   for(j in 1:nqtrs) {
@@ -50,31 +79,36 @@ for(i in 1:length(chg_fields)) {
   }
 }
 
-funy <- fun[dimension=="ARY"]
-funy <- merge(funy,y_restatement,by=c("ticker","calendardate"))
-rasset <- normalize_fields(paste0("re_diff_",c("assets","investments","tangibles","intangibles")),"assets",funy)
-rquick <- normalize_fields(paste0("re_diff_",c("receivables","payables","inventory","workingcapital")),"workingcapital",funy)
-rliab <- normalize_fields(paste0("re_diff_",c("debt","liabilities")),"liabilities",funy)
-requity <- normalize_fields(paste0("re_diff_",c("equity")),"equity",funy)
-rcash <- normalize_fields(paste0("re_diff_",c("fcf","ncfo")),"ncfo",funy)
-rincome <- normalize_fields(paste0("re_diff_",c("ebit","gp","netinc","revenue")),"revenue",funy)
-rfields <- c(rasset,rquick,rliab,requity,rcash,rincome)
-nyrs <- 3
-ydiff_fields <- c()
-for(i in 1:length(rfields)) {
-  for(j in 1:nyrs) {
-    nm <- paste0(rfields[i],"_",j,"_yr")
-    funy <- lookbehindqtr(nm,rfields[i],j*4,funy)
-    ydiff_fields <- c(ydiff_fields,nm)
-  }
+
+
+# ychg_fields <- c()
+# for(i in 1:length(chg_fields)) {
+#   for(j in 1:nyrs) {
+#     nm <- paste0(chg_fields[i],"_",j,"_yr")
+#     funy <- lookbehindqtr(nm,chg_fields[i],j,funy)
+#     ychg_fields <- c(ychg_fields,nm)
+#   }
+# }
+
+#Add prior field statistics
+for(i in 1:length(chg_fields)) {
+  field <- chg_fields[i]
+  fields <- c(field,paste0(field,"_",1:nqtrs,"_qtr"))
+  yfields <- fields[(0:(nqtrs%/%4))*4+1]
+  yfields <- yfields[!is.na(yfields)]
+  funq[[paste0(field,"_volatility_",nqtrs)]] <- rowvolatility(fields,funq)
+  funq[[paste0(field,"_trend_",nqtrs)]] <- rowtrend(fields,funq)
+  funq[[paste0(field,"_streak_qtr_",nqtrs)]] <- rowconseqchange(fields,funq)
+  funq[[paste0(field,"_streak_yr_",nqtrs)]] <- rowconseqchange(yfields,funq)
 }
 
-ychg_fields <- c()
-for(i in 1:length(chg_fields)) {
-  for(j in 1:nyrs) {
-    nm <- paste0(chg_fields[i],"_",j,"_yr")
-    funy <- lookbehindqtr(nm,chg_fields[i],j,funy)
-    ychg_fields <- c(ychg_fields,nm)
-  }
-}
+
+#Add competitor (industry) fields
+
+
+#Add future (target) fields
+
+
+#Write file
 write.csv(funq,"~/Core_US_Fundamentals/fun_quarter_4_23_2017.csv") #includes restatements and changes, but not industry comparisons or ratios
+# write.csv(funy,"~/Core_US_Fundamentals/fun_year_4_23_2017.csv") #includes restatements and changes, but not industry comparisons or ratios
